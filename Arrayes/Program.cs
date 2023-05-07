d53de875ee80de083514867275377f57f0b350b5
MZadonski <maria.zadons@tktk.ee>
2023-05-07 21:07:16 +0300

    create new app

---
+++ Arrayes/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+//string[] fruit = { "apples", "bananas", "oranges" };
+
+using System.Security.Cryptography.X509Certificates;
+
+string[] fruit = new string[3];
+
+fruit[0] = "apples";
+fruit[1] = "bananas";
+fruit[2] = "oranges";
+Console.WriteLine($"{fruit[0]}, {fruit[1]}, {fruit[2]}");
+
+int fruitArraysLenght = fruit.Length;
+
+Console.WriteLine($"There are {fruitArraysLenght} in your arry.");
+
+for ( int i = 0; i < fruitArraysLenght; i++ )
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+}
+foreach (string element in fruit)
+{
+    Console.WriteLine(element);
+}
---
+++ Week6StringsAndArrayes/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string hello = "we are the champions?";
+
+int stringLenght = hello.Length;
+
+//sting trimmedSting = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+//int wordCounter = 1;
+//for)int i = 0; i < hello.Lenght; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//       wordCounter++
+//    }
+//}
+// Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
